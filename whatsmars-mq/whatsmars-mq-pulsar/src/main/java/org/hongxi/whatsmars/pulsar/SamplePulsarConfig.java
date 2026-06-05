d0d46596a9347689c943ee6eb85c5f53848601ab
javahongxi <javahongxi@qq.com>
2026-06-05 23:59:16 +0800

    pulsar delay message

--- whatsmars-mq/whatsmars-mq-pulsar/src/main/java/org/hongxi/whatsmars/pulsar/SamplePulsarConfig.java
+++ whatsmars-mq/whatsmars-mq-pulsar/src/main/java/org/hongxi/whatsmars/pulsar/SamplePulsarConfig.java
@@ -6,10 +6,13 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.apache.pulsar.client.api.SubscriptionType;
 import org.springframework.pulsar.annotation.PulsarListener;
 import org.springframework.pulsar.core.PulsarTemplate;
 import org.springframework.pulsar.core.PulsarTopic;
 import org.springframework.pulsar.core.PulsarTopicBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration(proxyBeanMethods = false)
 public class SamplePulsarConfig {
@@ -35,10 +38,22 @@ public class SamplePulsarConfig {
 				template.send(TOPIC, new SampleMessage(i, "message:" + i));
 				logger.info("++++++PRODUCE MESSAGE:(" + i + ")------");
 			}
+
+			for (int i = 20; i < 30; i++) {
+				template.newMessage(new SampleMessage(i, "message:" + i))
+						.withTopic(TOPIC)
+						.withMessageCustomizer(mc -> {
+							mc.deliverAfter(5, TimeUnit.SECONDS);
+						}).send();
+				logger.info("++++++PRODUCE DELAY MESSAGE:(" + i + ")------");
+			}
 		};
 	}
 
-	@PulsarListener(topics = TOPIC)
+	/**
+	 * 延迟消息的消费端仅支持 Shared（共享订阅）模式
+	 */
+	@PulsarListener(topics = TOPIC, subscriptionType = SubscriptionType.Shared)
 	void consumeMessagesFromPulsarTopic(SampleMessage msg) {
 		logger.info("++++++CONSUME MESSAGE:(" + msg.id() + ")------");
 	}
