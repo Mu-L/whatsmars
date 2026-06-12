53c5ea649e99cf03071431e31af5fec668a8993d
javahongxi <javahongxi@qq.com>
2026-06-12 23:14:54 +0800

    Consumer端不需要@EnableDubbo

--- whatsmars-dubbo/whatsmars-dubbo-consumer-boot/src/main/java/org/hongxi/whatsmars/dubbo/demo/consumer/ConsumerApplication.java
+++ whatsmars-dubbo/whatsmars-dubbo-consumer-boot/src/main/java/org/hongxi/whatsmars/dubbo/demo/consumer/ConsumerApplication.java
@@ -1,6 +1,5 @@
 package org.hongxi.whatsmars.dubbo.demo.consumer;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by javahongxi on 2017/12/4.
  */
 @SpringBootApplication
-@EnableDubbo
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
